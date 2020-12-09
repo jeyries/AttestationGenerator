4866f5de59a8de8a5099a6f034063af5ec0327d8
Julien Eyries <julien@eyries.fr>
2020-12-09 17:26:51 +0100

    Initial Commit

---
+++ AttestationGenerator/AttestationGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AttestationGeneratorApp.swift
+//  AttestationGenerator
+//
+//  Created by Julien Eyriès on 09/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct AttestationGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
